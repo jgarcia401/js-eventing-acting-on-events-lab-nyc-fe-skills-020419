eb8b23b3d158e0bdff570f1eafa5ec4deafb92aa
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-26 00:04:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// 1- find the element
+
 const dodger = document.getElementById('dodger')
+
+
+function onKeyDown(){
+  
+  if (event.keycode === 37) {
+    
+    console.log(dodger.style.left)
+  }
+  
+  
+  
+}
+
+// 2. add event listener
+
+dodger.addEventListener('keydown', onKeyDown)
