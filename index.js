37243605c0f4b657ac087bf8ef4b3da6bdc02f5b
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-26 00:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const dodger = document.getElementById('dodger')
 function onKeyDown(event) {
   if (event.keyCode === 37) {
     // how do we actually move this left  
-    console.log(dodger.style.left)
+    
+    const currentPosition = parseInt(dodger.style.left)
+    
+    dodger.style.left = currentPosition - 5 + px
   }
 }
 
