92c7e0bf057baded5d9d8ce97c46a76f40f8e04f
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-26 00:36:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function onKeyDown(event) {
 function moveDodgerRight() {
   const currentPosition = parseInt(dodger.style.left)
   
-  if(currentPosition <= 400){
+  if(currentPosition <= 360){
     
     dodger.style.left = currentPosition + 5 + 'px'
   }
